e51f3dd116843b944be69d131d4fc11ab2759cd0
Doraemon <simplyaryash@gmail.com>
2026-05-10 12:33:04 +0530

    Update service-worker.js to reduce caching

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,32 @@
-const CACHE_NAME = "hw-tracker-v1";
+const CACHE_NAME = "hw-tracker-v999";
 
+// INSTALL
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "./",
-      ]);
-    })
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+// ACTIVATE
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => caches.delete(key))
+      )
+    )
   );
+
+  self.clients.claim();
+});
+
+// FETCH
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    fetch(event.request, { cache: "no-store" })
+      .catch(() => caches.match(event.request))
+
+  );
+
 });
